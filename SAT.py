262ab27c40702f72e65032bbe90f000928520785
Israel Pérez <pisrael252@gmail.com>
2017-01-09 20:56:37 +0100

    Update SAT.py

--- SAT.py
+++ SAT.py
@@ -2,14 +2,105 @@
 
 # Israel Perez
 # Contact: uservzk80@gmail.com
+# Contributors: Bernat Torres (bernatixer)
 
 import paramiko
 import os 
 from colorama import Fore, Back, Style
 
+ip = '192.168.1.100' #The ip of remote server
+port = 2222 #The port of the remote server
+user = "root" #The username for make connection
+passw = 'YWRtaW4=' #Password for make connection in base64
+passw_decode = passw.decode('base64')
+  
+_user_ = "@Uservzk80"
+__twitter__ = "https://twitter.com/uservzk80"
+_youtube_ = "https://www.youtube.com/channel/UCKOF5FGpvehulFOZkkxTWuw"
+
+s = paramiko.SSHClient()
+s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+channel_connection = s.connect(ip, username = user, password = passw_decode, port = port)
+
+commands = [
+  'ls: Shows files',
+  'current_dir: Shows the current directory',
+  'ifconfig: View configuration of the network',
+  'ram_cpu: Show the usage of RAM and CPU',
+  'ping: Ping a host',
+  'users_system: Shows logs',
+  'power_off: Shutdown the remote machine',
+  'help: Show commands'
+]
+
+def command_execution(user_action):
+
+  if 'help' == user_action.lower():
+    show_commands()
+
+  if 'ls' == user_action.lower():
+    command = "ls"
+    (stdin, stdout, stderr) = s.exec_command(command)
+    print "Files: "
+    for file in stdout:
+      print " - " + file[:-1]
+
+  if 'reboot' == user_action.lower():
+    command = "reboot"
+    (stdin, stdout, stderr) = s.exec_command(command)
+    print stdout.read()
+
+  if 'current_dir' == user_action.lower():
+    command = "pwd"
+    (stdin, stdout, stderr) = s.exec_command(command)
+    print stdout.read()
+
+  if 'ifconfig' == user_action.lower():
+    command = "ifconfig"
+    (stdin, stdout, stderr) = s.exec_command(command)
+    print stdout.read()
+
+  if 'ping' == user_action.lower():
+    ip_ping = raw_input('The IP for make a PING: ')
+    command = "ping " + ip_ping
+    (stdin, stdout, stderr) = s.exec_command(command)
+    print stdout.read()
+
+  if "ram_cpu" == user_action.lower():
+    command = "top"
+    (stdin, stdout, stderr) = s.exec_command(command)
+    print stdout.read()
+
+  if "power_on_system" == user_action.lower():
+    command = "who -b"
+    (stdin, stdout, stderr) = s.exec_command(command)
+    print stdout.read()
+ 
+  if "power_off" == user_action.lower():
+    command = "shutdown -r now"
+
+    yes_no = raw_input("power_off is not a recommended action ...\n (Y/N)")
+
+    if "y" == yes_no.lower():
+      (stdin, stdout, stderr) = s.exec_command(command)
+      print stdout.read()
+
+    if "n" == yes_no.lower():
+      print Fore.RED + 'Goodbye :D'
+
+  if "users_system" == user_action.lower():
+    command = "last"
+    (stdin, stdout, stderr) = s.exec_command(command)
+    print stdout.read()
+
+def show_commands():
+  print Fore.GREEN + "COMMANDS:"
+  for command in commands:
+    print " - " + command
+    
 class welcome():
 
-	print Fore.GREEN + '''
+  print Fore.GREEN + '''
 
  _____
 < SAT >
@@ -34,103 +125,19 @@ class welcome():
              /            _ \
 
 '''
-	print Fore.WHITE + ""
-	_user_ = "@Uservzk80"
-	__twitter__ = "https://twitter.com/uservzk80"
-	_youtube_ = "https://www.youtube.com/channel/UCKOF5FGpvehulFOZkkxTWuw"
-
-	print "Welcome to the program"	
-	print "This program was created by %s, please visit my social media content:" % (_user_)
-	print "		*YouTube -> %s\n		*Twitter -> %s" % (_youtube_, __twitter__)	
-
-
-	print Fore.GREEN + "COMMANDS:\n	*view_logs	*ifconfig	ram_cpu\n	*ping		*users_system	*power_of" #Aviable commands
-
-	user_action = raw_input(">>> ")
-
-	if "exit" == user_action.lower():
-		print Fore.RED + 'Goodbye :D'
-		exit()
-
-class data_connect():
-
-	ip = '192.168.1.100' #The ip of remote server
-	port = 2222 #The port of the remote server
-	user = "root" #The username for make connection
-	passw = 'YWRtaW4=' #Password for make connection in base64
-	passw_decode = passw.decode('base64')
-
-
-
-class make_connection():
-
-
-	connection = paramiko.Transport((data_connect.ip, data_connect.port)) #Make the connection with the ip and port 
-	connection.connect(username = data_connect.user, password = data_connect.passw_decode) # Make the connection with User and password decode
-	channel_connection = connection.open_session() #Open session and save it
-
-
-def command_execution():
-
-	if 'reboot' == welcome.user_action.lower():
-		command = "reboot"
-		
-		make_connection.channel_connection.exec_command(""+command) #Execute commmand
-		output = make_connection.channel_connection.makefile('rb, -1').readlines() #Read the lines with paramiko
-		print output
-
-
-	if 'view_logs' == welcome.user_action.lower(): #For execute command
-		command = "pwd"
-		make_connection.channel_connection.exec_command(""+command)
-	
-		output = make_connection.channel_connection.makefile('rb, -1').readlines()
-		print output
-
-	if 'ifconfig' == welcome.user_action.lower():
-		command = "ifconfig"
-		make_connection.channel_connection.exec_command(""+command) #Execute commmand
-		output = make_connection.channel_connection.makefile('rb, -1').readlines() #Read the lines with paramiko
-		print output # Print the output of the command execution
-
-	if 'ping' == welcome.user_action.lower():
-		ip_ping = raw_input('The IP for make a PING: ')
-		make_connection.channel_connection.exec_command("ping "+ip_ping)
-		output = make_connection.channel_connection.makefile('rb, -1').readlines()
-		print output
-
-	if "ram_cpu" == welcome.user_action.lower():
-		command = "top"
-		make_connection.channel_connection.exec_command(""+command) #Execute commmand
-		output = make_connection.channel_connection.makefile('rb, -1').readlines() #Read the lines with paramiko
-		print output # Print the output of the command execution
-
-	if "power_on_system" == welcome.user_action.lower():
-		command = "who -b"
-		make_connection.channel_connection.exec_command(""+command) #Execute commmand
-		output = make_connection.channel_connection.makefile('rb, -1').readlines() #Read the lines with paramiko
-		print output # Print the output of the command execution
- 
-	if "power_of" == welcome.user_action.lower():
-		command = "shutdown -r now"
-
-		yes_no = raw_input("power_of is not a recommended action ...\n (Y/N)")
-
-		if "y" == yes_no.lower():
-			make_connection.channel_connection.exec_command(""+command) #Execute commmand
-			output = make_connection.channel_connection.makefile('rb, -1').readlines() #Read the lines with paramiko
-			print output # Print the output of the command execution
-
-		if "n" == yes_no.lower():
-			print Fore.REd + 'Goodbye :D'
-
-	if "users_system" == welcome.user_action.lower():
-		command = "last"
-		make_connection.channel_connection.exec_command(""+command) #Execute commmand
-		output = make_connection.channel_connection.makefile('rb, -1').readlines() #Read the lines with paramiko
-		print output # Print the output of the command execution
-
+  print Fore.WHITE + ""
+  print "Welcome to the program"  
+  print "This program was created by %s, please visit my social media content:" % (_user_)
+  print "   *YouTube -> %s\n    *Twitter -> %s" % (_youtube_, __twitter__)  
+  print "Contributor: @BenatTorres - GitHub: https://github.com/bernatixer"
+  
+  show_commands()
+  
+  while 1:
+    user_action = raw_input(">>> ")
+    if "exit" == user_action.lower():
+      print Fore.RED + 'Goodbye :D'
+      exit()
+    command_execution(user_action)
 
 welcome()
-make_connection()
-command_execution()
